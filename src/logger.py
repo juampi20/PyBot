343f59efa5d29f4ac1880f67cefffa915d28020b
juampi20 <juampigarcia2014@gmail.com>
2020-09-15 00:57:39 -0300

    Creating logger

---
+++ src/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+# TODO: Create Logger for bot
+
+class Logger:
+    def __init__(self):
+        # Create logger
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('[%(levelname)s]: %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
